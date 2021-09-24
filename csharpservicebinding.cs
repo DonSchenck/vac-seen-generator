508fd7008589937a006328586bbbfdd47e9b5676
Don Schenck <don.schenck@gmail.com>
2021-09-24 16:35:58 -0400

    debugging

--- csharpservicebinding.cs
+++ csharpservicebinding.cs
@@ -1,35 +1,40 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 public class ServiceBinding {
-  public static List<Tuple> Main (string[] args) {
+  
+  // var tupleList = new List<(string Firstname, string Lastname)>;
+
+  public List<(string key,string value)> Main (string[] args) {
 
     var bindingDirectory = Environment.GetEnvironmentVariable("SERVICE_BINDING_ROOT");
     return ProcessDirectoryTree(bindingDirectory);
   }
 
 
-  public static List<Tuple> ProcessDirectoryTree(string directory)
+  public static List<(string key,string value)> ProcessDirectoryTree(string directory)
   {
     // Walk down the directory tree and for each file, use the filename
     // as the key and the contents as the value, and add this
     // key-value pair to a list of key-value pairs.
     // At the end, return the list.
 
-    List<Tuple> l();
+    List<(string key,string value)> l = new List<(string key,string value)>();
 
     foreach (string f in Directory.GetFiles(directory))
     {
-        var r = GetFileContents(Path.GetFileName(f),"");
-        l.add(r.key,r.value);
+        var r = GetFileContents(Path.GetFileName(f));
+        l.Add(r);
     } 
 
     foreach (string d in Directory.GetDirectories(directory))
     {
       ProcessDirectoryTree(d);
     }
+    return l;
   }
-  (string key, string value) GetFileContents(string filename)
+  private static (string key, string value) GetFileContents(string filename)
   {
     // Get contents of file
     string v = System.IO.File.ReadAllText(filename);
